3b890f8d92c5196151d2f2e44523014358384324
Riccardo Canella <canella_riccardo@icloud.com>
2021-11-06 23:50:37 +0100

    Initial Commit

---
+++ A Selfie Every Day/A_Selfie_Every_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  A_Selfie_Every_DayApp.swift
+//  A Selfie Every Day
+//
+//  Created by canella riccardo on 06/11/21.
+//
+
+import SwiftUI
+
+@main
+struct A_Selfie_Every_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A Selfie Every Day/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  A Selfie Every Day
+//
+//  Created by canella riccardo on 06/11/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
